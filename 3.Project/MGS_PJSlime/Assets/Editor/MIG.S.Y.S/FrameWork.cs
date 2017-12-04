40715686ac47a19735dd666390155b6bee9a30cc
s436911 <s436911@yahoo.com.tw>
2017-12-04 20:24:28 +0800

    123
    123

---
+++ 3.Project/MGS_PJSlime/Assets/Editor/MIG.S.Y.S/FrameWork.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public abstract class FrameWork : EditorWindow {
+
+    void OnEnable() {
+        UpdateInfo();
+    }
+
+    void OnHierarchyChange() {
+        UpdateInfo();
+    }
+
+    protected abstract void UpdateInfo();
+
+    protected void BeginH(){
+        GUILayout.BeginHorizontal();
+    }
+
+    protected void BeginV() {
+        GUILayout.BeginVertical();
+    }
+
+    protected void BeginV(string ask) {
+        GUILayout.BeginVertical(ask);
+    }
+
+    protected void EndH() {
+        GUILayout.EndHorizontal();
+    }
+
+    protected void EndV() {
+        GUILayout.EndVertical();
+    }
+}
---
+++ 3.Project/MGS_PJSlime/Assets/Editor/MIG.S.Y.S/MigEditorMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class MigEditorMenu : EditorWindow {
+
+
+    private static void FileCheck() {
+        string infoName;
+        string assetPath = "Assets/Resources/Info/";
+
+        if (!Directory.Exists(assetPath)) {
+            Directory.CreateDirectory(assetPath);
+        }
+        
+        infoName = "RegionInfo";
+        if(!File.Exists(assetPath + infoName + ".asset")) {
+
+            //RegionInfo regionInfo = ScriptableObject.CreateInstance<RegionInfo>();
+            //AssetDatabase.CreateAsset(regionInfo, assetPath + infoName + ".asset");
+        }
+    }
+
+
+    [MenuItem("MigEditor/方格編輯功能/區域編輯器", false, 0)]
+    static void ModelCreater() {
+        FileCheck();
+        //EditorWindow.GetWindow(typeof(RegionEditor));
+    }
+}
